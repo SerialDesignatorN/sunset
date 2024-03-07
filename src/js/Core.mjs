a940bf569e7b875bdb868a5f90ed0991868ed45c
SerialDesignatorN <zeanfender11@gmail.com>
2024-03-07 20:27:28 +0700

    Fix broken variables (fix soon

--- src/js/Core.mjs
+++ src/js/Core.mjs
@@ -1,6 +1,6 @@
 import ScreenDrawer from './modules/ScreenDrawer.mjs'
 
-var cccc = 0;numberinuse = 0;speednumber = 4;mrmode = 0;mrgate = 0;mrin = 0;mrout = 0;mrp1 = 0;mrp2 = 0;mrp3 = 0;mrp4 = 0;fps = 100;x = 10;y = 10;
+var cccc = 0,numberinuse = 0,speednumber = 4,mrmode = 0,mrgate = 0,mrin = 0,mrout = 0,mrp1 = 0,mrp2 = 0,mrp3 = 0,mrp4 = 0,fps = 100,x = 10,y = 10;
 var wires = 0
 var gates = 0
 let isOkayLogic = true
