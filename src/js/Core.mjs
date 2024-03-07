72cb5a5c0b9685935724cf65849da8bd916cfbe4
SerialDesignatorN <zeanfender11@gmail.com>
2024-03-07 20:26:34 +0700

    The start

--- src/js/Core.mjs
+++ src/js/Core.mjs
@@ -1,4 +1,8 @@
 import ScreenDrawer from './modules/ScreenDrawer.mjs'
 
+var cccc = 0;numberinuse = 0;speednumber = 4;mrmode = 0;mrgate = 0;mrin = 0;mrout = 0;mrp1 = 0;mrp2 = 0;mrp3 = 0;mrp4 = 0;fps = 100;x = 10;y = 10;
+var wires = 0
+var gates = 0
+let isOkayLogic = true
 const drawer = new ScreenDrawer()
 drawer.DrawScreenOnNew()
